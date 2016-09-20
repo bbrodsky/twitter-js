05654dfde3753682fe8e4f0f322387714ca65a6d
bbrodsky <bbrodsky@gmail.com>
2016-09-20 11:49:01 -0400

    An excellent time for a commit

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
 var express = require( 'express' );
 const volleyball = require('volleyball');
 var nunjucks = require('nunjucks');
-
+var routes = require('./routes/');
 
 
 var app = new express(); // creates an instance of an express application
 app.use(volleyball);
-// app.use(function (req, res, next) {
-//   console.log(req.method, req.url);
-//   next();
-// });
+app.use('/', routes);
+nunjucks.configure('views');
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
 
-// app.use('/news', function(req,res,next){
-//   console.log("Now here's the news...");
-//   next();
-// });
 
 app.get('/', function(req, res, next) {
   // res.set('Content-Type', 'text/plain');
-  var people = [{name: "Full"}, {name: "Stacker"}, {name: "son"}];
-  res.render('index', {title: "Hall of Fame", people: people});
-  
+  // var people = [{name: "Full"}, {name: "Stacker"}, {name: "son"}];
+  // res.render('index', {title: "Hall of Fame", people: people});
+
 });
 
 app.listen(3000, function(){
@@ -41,9 +37,7 @@ nunjucks.render('index.html', locals, function (err, output) {
 	console.log(output);
 });
 
-nunjucks.configure('views');
-app.set('view engine', 'html');
-app.engine('html', nunjucks.render);
+
 
 
 
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var tweetBank = require('../tweetBank');
+
+router.use(express.static('public'));
+
+router.get('/', function(req, res) {
+  var tweets = tweetBank.list();
+  res.render('index', {tweets: tweets});
+});
+
+module.exports = router;
